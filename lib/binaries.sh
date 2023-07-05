931edeab7ca30e5ea5d3fa8adb2ca9317bf816c4
Christoffer Åhrling <christoffer@nilpath.se>
2023-07-05 15:48:27 +0200

    make it compatible with my dokku installation

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -1,61 +1,129 @@
-needs_resolution() {
-  local semver=$1
-  if ! [[ "$semver" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    return 0
+#!/usr/bin/env bash
+
+RESOLVE="$BP_DIR/lib/vendor/resolve-version-$(get_os)"
+
+resolve() {
+  local binary="$1"
+  local versionRequirement="$2"
+  local n=0
+  local output
+
+  # retry this up to 5 times in case of spurious failed API requests
+  until [ $n -ge 5 ]
+  do
+    # if a user sets the HTTP_PROXY ENV var, it could prevent this from making the S3 requests
+    # it needs here. We can ignore this proxy for aws urls with NO_PROXY. Some environments
+    # require a proxy for all HTTP requests, so the NO_PROXY ENV var should be set outside the
+    # script by the user
+    # see testAvoidHttpProxyVersionResolutionIssue test and README
+    if output=$($RESOLVE "$binary" "$versionRequirement"); then
+      echo "$output"
+      return 0
+    # don't retry if we get a negative result
+    elif [[ $output = "No result" ]]; then
+      return 1
+    elif [[ $output == "Could not parse"* ]] || [[ $output == "Could not get"* ]]; then
+      return 1
+    else
+      n=$((n+1))
+      # break for a second with a linear backoff
+      sleep $((n+1))
+    fi
+  done
+
+  return 1
+}
+
+install_yarn() {
+  local dir="$1"
+  local version=${2:-1.22.x}
+  local number url code resolve_result
+
+  if [[ -n "$YARN_BINARY_URL" ]]; then
+    url="$YARN_BINARY_URL"
+    echo "Downloading and installing yarn from $url"
   else
-    return 1
+    echo "Resolving yarn version $version..."
+    resolve_result=$(resolve yarn "$version" || echo "failed")
+
+    if [[ "$resolve_result" == "failed" ]]; then
+      fail_bin_install yarn "$version"
+    fi
+
+    read -r number url < <(echo "$resolve_result")
+
+    echo "Downloading and installing yarn ($number)"
+  fi
+
+  code=$(curl "$url" -L --silent --fail --retry 5 --retry-max-time 15 -o /tmp/yarn.tar.gz --write-out "%{http_code}")
+
+  if [ "$code" != "200" ]; then
+    echo "Unable to download yarn: $code" && false
+  fi
+  rm -rf "$dir"
+  mkdir -p "$dir"
+  # https://github.com/yarnpkg/yarn/issues/770
+  if tar --version | grep -q 'gnu'; then
+    tar xzf /tmp/yarn.tar.gz -C "$dir" --strip 1 --warning=no-unknown-keyword
+  else
+    tar xzf /tmp/yarn.tar.gz -C "$dir" --strip 1
+  fi
+  chmod +x "$dir"/bin/*
+
+  if $YARN_2; then
+    echo "Using yarn $(yarn --version)"
+  else
+    echo "Installed yarn $(yarn --version)"
   fi
 }
 
 install_nodejs() {
-  local version="$1"
-  local dir="$2"
+  local version=${1:-12.x}
+  local dir="${2:?}"
+  local code os cpu resolve_result
 
-  if needs_resolution "$version"; then
-    echo "Resolving node version ${version:-(latest stable)} via semver.io..."
-    local version=$(curl --silent --get --data-urlencode "range=${version}" https://semver.herokuapp.com/node/resolve)
+  os=$(get_os)
+  cpu=$(get_cpu)
+
+
+  echo "Resolving node version $version..."
+  resolve_result=$(resolve node "$version" || echo "failed")
+
+  read -r number url < <(echo "$resolve_result")
+
+  if [[ "$resolve_result" == "failed" ]]; then
+    fail_bin_install node "$version"
   fi
 
-  echo "Downloading and installing node $version..."
-  local download_url="http://s3pository.heroku.com/node/v$version/node-v$version-$os-$cpu.tar.gz"
-  curl "$download_url" --silent --fail -o /tmp/node.tar.gz || (echo "Unable to download node $version; does it exist?" && false)
-  tar xzf /tmp/node.tar.gz -C /tmp
-  mv /tmp/node-v$version-$os-$cpu/* $dir
-  chmod +x $dir/bin/*
-}
+  echo "Downloading and installing node $number..."
 
-install_iojs() {
-  local version="$1"
-  local dir="$2"
 
-  if needs_resolution "$version"; then
-    echo "Resolving iojs version ${version:-(latest stable)} via semver.io..."
-    version=$(curl --silent --get --data-urlencode "range=${version}" https://semver.herokuapp.com/iojs/resolve)
+  code=$(curl "$url" -L --silent --fail --retry 5 --retry-max-time 15 -o /tmp/node.tar.gz --write-out "%{http_code}")
+
+  if [ "$code" != "200" ]; then
+    echo "Unable to download node: $code" && false
   fi
-
-  echo "Downloading and installing iojs $version..."
-  local download_url="https://iojs.org/dist/v$version/iojs-v$version-$os-$cpu.tar.gz"
-  curl "$download_url" --silent --fail -o /tmp/node.tar.gz || (echo "Unable to download iojs $version; does it exist?" && false)
   tar xzf /tmp/node.tar.gz -C /tmp
-  mv /tmp/iojs-v$version-$os-$cpu/* $dir
-  chmod +x $dir/bin/*
+  rm -rf "${dir:?}"/*
+  mv /tmp/node-v"$number"-"$os"-"$cpu"/* "$dir"
+  chmod +x "$dir"/bin/*
 }
 
 install_npm() {
+  local npm_version
   local version="$1"
+  local dir="$2"
+  npm_version="$(npm --version)"
 
   if [ "$version" == "" ]; then
-    echo "Using default npm version: `npm --version`"
+    echo "Using default npm version: $npm_version"
+  elif [[ "$npm_version" == "$version" ]]; then
+    echo "npm $npm_version already installed with node"
   else
-    if needs_resolution "$version"; then
-      echo "Resolving npm version ${version} via semver.io..."
-      version=$(curl --silent --get --data-urlencode "range=${version}" https://semver.herokuapp.com/npm/resolve)
+    echo "Bootstrapping npm $version (replacing $npm_version)..."
+    if ! npm install --unsafe-perm --quiet -g "npm@$version" 2>@1>/dev/null; then
+      echo "Unable to install npm $version; does it exist?" && false
     fi
-    if [[ `npm --version` == "$version" ]]; then
-      echo "npm `npm --version` already installed with node"
-    else
-      echo "Downloading and installing npm $version (replacing version `npm --version`)..."
-      npm install --unsafe-perm --quiet -g npm@$version 2>&1 >/dev/null
-    fi
+    echo "npm $version installed"
   fi
 }
